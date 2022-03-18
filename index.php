85d6bc3edd658a3258856117e2ac0bcc6f1acfb0
Arslan Ahsan Jalib <101581096+Arslan-Ahsan-Jalib-Dev@users.noreply.github.com>
2022-03-18 22:07:47 +0500

    Html structure Updated

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!-- meta tag starts-->
+    <meta charset="UTF-8">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- meta tag ends-->
+    <title>High Consulting</title>
+    <!-- bootstrap cdn starts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- bootstrap cdn ends -->
+    <!-- font owsome cdn starts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- font owsome cdn ends-->
+    <!--externa css starts-->
+    <link rel="stylesheet" type="text/css" href="assets/stylesheet/styles.css">
+    <!--externa css ends-->
+</head>
+<body>
+  
+    <section class="">
+       <div class="container">
+         <div class="row mt-5">
+        <div class="col-md-4">
+            <div class="features-items-wrapper text-center">
+                <div class="icon-wrapper">
+                    <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                </div>
+                <div class="special-text">
+                    <h3 class="item-heading">Open An Account</h3>
+                    <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?
+                    </p>
+                    <div class="text-center">
+                        <span class="click btn btn-link"><i class="fa fa-angle-down" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="features-items-wrapper text-center ">
+                <div class="icon-wrapper">
+                    <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                </div>
+                <div class="special-text">
+                    <h3 class="item-heading">Give Intrest</h3>
+                    <p class="">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?
+                    </p>
+                    <div class="text-center">
+                        <span class="click btn btn-link"><i class="fa fa-angle-down" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="features-items-wrapper">
+                <div class="icon-wrapper text-center">
+                    <i class="fa fa-bar-chart" aria-hidden="true"></i>
+                </div>
+                <div class="special-text">
+                    <h3 class="item-heading text-center">Get Deposit</h3>
+                    <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat vero reprehenderit adipisci repellendus esse aliquid deleniti natus id cumque, expedita molestiae dolorem! Esse, labore? Laborum at dolor quod iure?
+                    </p>
+                    <div class="text-center">
+                        <span class="click btn btn-link"><i class="fa fa-angle-down" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+       </div>
+    </section>
+    <!-- scripts here starts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="assets/javascript/script.js"></script>
+  <!-- scripts here ends -->
+</body>
+</html>
